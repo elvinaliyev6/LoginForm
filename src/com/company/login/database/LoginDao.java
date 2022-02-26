664fd058fa6d455c8f098061038ccd7e9f3a0abb
elvinaliyev6 <aliyevaliyevelvin@gmail.com>
2022-02-26 10:55:30 +0400

    login form

--- src/com/company/login/database/LoginDao.java
+++ src/com/company/login/database/LoginDao.java
@@ -10,6 +10,26 @@ import java.sql.ResultSet;
 public class LoginDao {
 
     public static void main(String[] args) throws Exception{
+        Class.forName("com.mysql.jdbc.Driver");
+
+
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/employees",
+                "root", "12345")) {
+            LoginBean login =null;
+            PreparedStatement stmt = connection.prepareStatement("select * from login where username=? and password=?");
+            stmt.setString(1,"Elvin");
+            stmt.setString(2, "12345");
+            stmt.execute();
+            ResultSet rs = stmt.getResultSet();
+            while(rs.next()){
+                login.setUsername(rs.getString("username"));
+                login.setPassword(rs.getString("password"));
+            }
+            System.out.println(login.getUsername()+", parol is "+login.getPassword());
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
 
     }
 
@@ -20,7 +40,7 @@ public class LoginDao {
 
         try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/employees",
                 "root", "12345")) {
-            PreparedStatement stmt = connection.prepareStatement("select * from login where username=? and pasword=?");
+            PreparedStatement stmt = connection.prepareStatement("select * from login where username=? and password=?");
             stmt.setString(1, login.getUsername());
             stmt.setString(2, login.getPassword());
             System.out.println(stmt);
--- src/main/webapp/login.jsp
+++ src/main/webapp/login.jsp
@@ -10,9 +10,11 @@
 <html>
 <head>
     <title>Login Form</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
-    <div align="center">
+<div align="center">
+    <div class="form-group">
         <h1>Employee Login Form</h1>
         <form action="<%=request.getContextPath()%>/login" method="POST">
             <table style="width: 20%">
@@ -20,13 +22,18 @@
                     <td>Username</td>
                     <td><input type="text" name="username"/></td>
                 </tr>
-                <tr>
-                    <td>Password</td>
-                    <td><input type="text" name="password"/></td>
-                </tr>
-            </table>
-            <input type="submit" value="Submit"/>
-        </form>
     </div>
+    <div class="form-group">
+        <tr>
+            <td>Password</td>
+            <td><input type="text" name="password"/></td>
+        </tr>
+        </table>
+    </div>
+    <div>
+        <input type="submit" value="Submit" class="btn btn-primary"/>
+    </div>
+    </form>
+</div>
 </body>
 </html>
